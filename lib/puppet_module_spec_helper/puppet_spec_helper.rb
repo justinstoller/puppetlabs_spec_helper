8524b71deb7e20fc596d9f5a7655dad60ab9e970
Branan Purvine-Riley <branan@puppetlabs.com>
2012-05-29 15:30:07 -0700

    Add a couple of helper functions

--- lib/puppet_module_spec_helper/puppet_spec_helper.rb
+++ lib/puppet_module_spec_helper/puppet_spec_helper.rb
@@ -187,7 +187,19 @@ RSpec.configure do |config|
 
 end
 
-# copied directly from the BizDev spec helper
+# TODO: These need a better home, but tests tend to assume they're in the global
+# namespace
+def param_value(subject, type, title, param)
+  subject.resource(type, title).send(:parameters)[param.to_sym]
+end
+
+def verify_contents(subject, title, expected_lines)
+  content = subject.resource('file', title).send(:parameters)[:content]
+  (content.split("\n") & expected_lines).should == expected_lines
+end
+
+# For modules with dependencies and/or that use rspec-puppet, this code
+# properly initializes the modulepath and manifests dir 
 Puppet.parse_config
 puppet_module_path = Puppet[:modulepath]
 
