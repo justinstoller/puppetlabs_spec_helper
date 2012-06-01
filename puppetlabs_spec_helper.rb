012359103f033a33ec1cc1435de1745fe9c2396c
Justin Stoller <justin@puppetlabs.com>
2012-06-01 15:50:54 -0700

    throw deprecation warnings

--- puppetlabs_spec_helper.rb
+++ puppetlabs_spec_helper.rb
@@ -1,3 +1,5 @@
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'puppetlabs_spec/rspec'
+
+puts "Using 'PROJECT_ROOT/puppetlabs_spec_helper' is deprecated, please install as a gem and require 'puppetlabs_spec/rspec' instead"
